c3fe8f1291c8ec6952f55df56c837e4fdc63d530
Sonam Shah <sonam.shah1687@gmail.com>
2025-08-13 10:09:36 -0500

    Add validation and custom validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,16 @@
 class Event < ApplicationRecord
-	belongs_to :person
+	belongs_to :person, dependent: :destroy
+
+	validates :title, :location, presence: true
+	validate :date_cannot_be_in_past
+
+	private
+
+	def date_cannot_be_in_past
+		return errors.add(:date, "cannot be empty") if date.nil?
+		
+		unless date > DateTime.now
+			errors.add(:date, "cannot be in past")
+		end
+	end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,6 @@
 class Person < ApplicationRecord
 	has_many :events, dependent: :destroy
+
+	validates :name, presence: true
+	validates :email, presence: true, format: {with: /\A\S+@\S+\.\w+\z/} , uniqueness: { case_sensitive: false }
 end
