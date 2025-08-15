22c84cc0d4c743cd38ab7cb3fd0eab2fadd1aeb0
Sonam Shah <sonam.shah1687@gmail.com>
2025-08-15 10:54:54 -0500

    refactor: refactor model validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,16 +1,10 @@
 class Event < ApplicationRecord
-	belongs_to :person, dependent: :destroy
+	belongs_to :person
 
 	validates :title, :location, presence: true
-	validate :date_cannot_be_in_past
+	validates :date, presence: true, comparision: { greater_than_or_equal_to: Date.current}
 
-	private
-
-	def date_cannot_be_in_past
-		return errors.add(:date, "cannot be empty") if date.nil?
-
-		unless date >= Date.today
-			errors.add(:date, "cannot be in past")
-		end
-	end
+	scope :upcoming, -> { where(date: Date.current..).order(date: :asc) }
+	scope :next_seven_days, -> { where(date: Date.current..Date.current + 7).order(date: :asc) }
+  scope :past, -> { where(date: ...Date.current).order(date: :desc) }
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,16 @@
 class Person < ApplicationRecord
 	has_many :events, dependent: :destroy
 
+	before_validation :normalize_email
+
 	validates :name, presence: true
-	validates :email, presence: true, format: {with: /\A\S+@\S+\.\w+\z/} , uniqueness: { case_sensitive: false }
+	validates :email, presence: true, 
+	                  format: {with: /\A\S+@\S+\.\w+\z/} , 
+	                  uniqueness: { case_sensitive: false }
+
+	private
+
+  def normalize_email
+  	self.email = email.to_s.strip.downcase
+  end
 end
